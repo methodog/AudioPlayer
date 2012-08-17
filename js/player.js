721f546d8067cdd5fe56e4db4730e385b163be7d
methodog <sittingbottom@gmail.com>
2012-08-17 15:30:55 +0100

    in progress

--- js/player.js
+++ js/player.js
@@ -1,53 +1,33 @@
-$(function(){
-    initAudio();
-});
+jQuery(document).ready(function() {
 
-function initAudio(){
+    $('#player').append('<audio preload="metadata"><source src="test.wav" type="audio/wav"></source><source src="test.ogg" type="audio/ogg"></source></audio>');
+    
     var supportsAudio = !!document.createElement('audio').canPlayType,
-        audio, loading, knobProg, knobVol, time, loaded = false, manualSeek = false;
+        audio, loaded = 0, manualSeek = 0, vol = 0.7;
 
     if( supportsAudio ){
-        var player = '\
-                <p id="player">\
-                    <span id="play" />\
-                    <span id="prog">\
-                        <span id="loading" />\
-                        <span class="knob ui-slider-handle" />\
-                    </span>\
-                    <span id="vol">\
-                        <span class="knob ui-slider-handle" />\
-                    </span>\
-                    <span id="time" />\
-                    <audio preload="metadata">\
-                        <source src="test.wav" type="audio/wav"></source>\
-                        <source src="test.ogg" type="audio/ogg"></source>\
-                    </audio>\
-                </p>';
-        
-        $('body').append(player);
-        
         audio = $('audio').get(0);
-        loading = $('#loading');
-        knobProg = $('#prog>.knob');
-        knobVol = $('#vol>.knob');
-        time = $('#time');
+                
+        audio.volume = vol;
         
         $('#vol').slider({
+            min:0.4,
             max:1,
-            step:0.1,
-            animate:true,					
-            stop:function(e,ui){
+            step:0.01,
+            value:vol,
+            animate:true,
+            slide:function(e,ui){
                 audio.volume = ui.value;
             }
         });
         
         if( (audio.buffered !== undefined) && (audio.buffered.length !== 0) ){
-            $(audio).on('progress', function(){
+            $(audio).on('progress', function(){console.log('yo');
                 var loaded = parseInt(((audio.buffered.end(0)/audio.duration)*100),10);
-                loading.css({width:loaded+'%'});
+                $('#loading').css({'width':loaded+'%'});
             });
         }else{
-            loading.remove();
+            $('#loading').remove();
         }
         
         $(audio)
@@ -57,35 +37,33 @@ function initAudio(){
                     mins = Math.floor(rem/60,10),
                     secs = rem-mins*60;
             
-            time.text('-'+mins+':'+ (secs<10 ? '0'+secs : secs));
-            if( !manualSeek ){ knobProg.css({left:pos+'%'}); }
+            $('#time').text('-'+mins+':'+ (secs<10 ? '0'+secs : secs));
+            if( !manualSeek ){ $('#prog>.knob').css({left:pos+'%'}); }
             if( !loaded ){
                 loaded = true;
                 $('#prog').slider({
                     step:0.01,
                     max:audio.duration,
-                    animate:true,					
-                    slide:function(){							
+                    animate:true,
+                    slide:function(){
                         manualSeek = true;
                     },
                     stop:function(e,ui){
-                        manualSeek = false;					
+                        manualSeek = false;
                         audio.currentTime = ui.value;
                     }
                 });
             }
         }).on('play',function(){
-            $('#play').addClass('playing');		
+            $('#play').addClass('playing');
         }).on('pause ended', function(){
-            $('#play').removeClass('playing');		
+            $('#play').removeClass('playing');
         });		
         
-        $('#play').click(function(){			
+        $('#play').click(function(){
             if( audio.paused ){ audio.play(); } 
             else{ audio.pause(); }			
         });
-
     }
     
-    
-}
+});
