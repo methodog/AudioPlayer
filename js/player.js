db438b7abe581c3b07357e2b61efbe03bcffda9b
methodog <sittingbottom@gmail.com>
2012-08-22 12:15:28 +0100

    tidy

--- js/player.js
+++ js/player.js
@@ -57,45 +57,28 @@ jQuery(document).ready(function() {
                 manualSeek = 0, 
                 vol = 0.7;
             audio.volume = vol;
-            $('#vol').slider({
-                min:0.4,
-                max:1,
-                step:0.01,
-                value:vol,
-                animate:true,
-                slide:function(e,ui){
-                    audio.volume = ui.value;
-                }
+            $('#vol').slider({ min:0.4, max:1, step:0.01, value:vol, animate:true, 
+                slide:function(e,ui){ audio.volume = ui.value; }
             });
-            $(audio).on('loadeddata', function(){
+            $(audio)
+            .on('loadeddata', function(){
                 var t = parseInt(audio.duration,10),
                     m = Math.floor(t/60,10),
                     s = t-m*60;
                 $('#time').text((m<10?'0'+m:m)+':'+(s<10?'0'+s:s));
-            }).on('timeupdate', function(){
+            })
+            .on('timeupdate', function(){
                 var pos = (audio.currentTime/audio.duration)*100;
                 if( !manualSeek ){ $('#progknob').css({left:pos+'%'}); }
-                $('#prog').slider({
-                    step:0.01,
-                    max:audio.duration,
-                    animate:true,
-                    slide:function(){
-                        manualSeek = true;
-                    },
-                    stop:function(e,ui){
-                        manualSeek = false;
-                        audio.currentTime = ui.value;
-                    }
+                $('#prog').slider({ step:0.01, max:audio.duration, animate:true, 
+                    slide:function(){ manualSeek = true; },
+                    stop:function(e,ui){ manualSeek = false; audio.currentTime = ui.value; }
                 });
-            }).on('play',function(){ 
-                $('#play').addClass('playing');
-            }).on('pause ended', function(){
-                $('#play').removeClass('playing');
-            });		
-            $('#play').click(function(){
-                if( audio.paused ){ audio.play(); } 
-                else{ audio.pause(); }			
-            });
+            })
+            .on('play', function(){ $('#play').addClass('playing'); })
+            .on('pause ended', function(){ $('#play').removeClass('playing'); })
+            ;		
+            $('#play').click(function(){ if( audio.paused ){ audio.play(); }else{ audio.pause(); } });
         }else{
             $('audio').attr('controls','controls');
             $('#dash').hide();
