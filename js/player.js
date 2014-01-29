066398e80fc43a910d60b17b496f9fddcf669fdf
methodog <sittingbottom@gmail.com>
2014-01-29 15:12:06 +0000

    extend media support to video

--- js/player.js
+++ js/player.js
@@ -82,43 +82,54 @@ jQuery(document).ready(function() {
     };
 
     $.get('./site.xml', function(xml){
-        var $site = $(xml).children('site'),
-            header = $site.children('home').children('name').text(),
-            $link = $site.children('home').children('link'),
-            imgs = $site.find('menuimage').length,
-            audio = $('audio')[0];
+        var $site = $(xml).find('site'),
+            header = $site.find('home').find('name').text(),
+            $link = $site.find('home').find('link'),
+            imgs = $site.find('menuimage').length;
         $('#menu').append('<h1>'+header+'</h1>');
         $link.each(function(){
             var t_id = $(this).text(),
-                $xml = $site.children(t_id),
-                title = $xml.children('menuname').text(),
-                img = $xml.children('menuimage').text(),
-                file = $xml.children('video').text(),
-                t_title = $xml.children('name').text();
+                $xml = $site.find(t_id),
+                title = $xml.find('menuname').text(),
+                img = $xml.find('menuimage').text(),
+                media = $xml.find('video').length > 0 ? 'video':'audio',
+                file = $xml.find(media).text(),
+                transcript = $xml.find('transcript').text(),
+                t_title = $xml.find('name').text();
             img = img?img:'css/img/blank.gif';
-            $('#menu').append($('<p/>').append('<a class="menu button" id="'+t_id+'" data-file="'+file+'" href="javascript:void(0)"><img alt="" src="'+img+'"'+(imgs?'':' class="none"')+'/><span>'+title+'</span></a>'));
-            $.get(file+'.xml', function(t_xml){
-                var $t_parts = $(t_xml).find('p'), 
-                    t_html = '',
-                    t_t = [];
-                $t_parts.each(function(){
-                    var t = $(this).attr('begin').split(':').reverse(), s = 0;
-                    for(var i=0; i<t.length; ++i){ s += i==0? parseInt(t[i]):parseInt(t[i]*60*i); }
-                    t_t.push(s);
-                    t_html += '<p class="'+s+'">'+$(this).text()+'</p>';
-                });
-                $('#transcript').append('<div class="'+t_id+'" data-times="'+t_t.join()+'"><h2>'+t_title+'</h2>'+t_html+'</div>');
-            },'html');
+            $('#menu').append($('<p/>').append('<a class="menu button" id="'+t_id+'" data-file="'+file+'" data-media="'+media+'" href="javascript:void(0)"><img alt="" src="'+img+'"'+(imgs?'':' class="none"')+'/><span>'+title+'</span></a>'));
+            if( !!transcript ){
+                $.get(transcript, function(t_xml){
+                    var $t_parts = $(t_xml).find('p'), 
+                        t_html = '',
+                        t_t = [];
+                    $t_parts.each(function(){
+                        var t = $(this).attr('begin').split(':').reverse(), s = 0;
+                        for(var i=0; i<t.length; ++i){ s += i==0? parseInt(t[i]):parseInt(t[i]*60*i); }
+                        t_t.push(s);
+                        t_html += '<p class="'+s+'">'+$(this).text()+'</p>';
+                    });
+                    $('#transcript').append('<div class="'+t_id+'" data-times="'+t_t.join()+'"><h2>'+t_title+'</h2>'+t_html+'</div>');
+                },'html');
+            }
         });
         $('#menu').on('click', '.menu.button', function(){
             var file = $(this).data('file'),
+                media = $(this).data('media'),
                 t_id = this.id;
-            $('a.menu.button').removeClass('playing');
+            $('.menu.button').removeClass('playing');
             $(this).addClass('playing');
-            $(audio).children('source').remove();
-            //~ $(audio).append('<source src="'+file+'.mp3" type="audio/mpeg"></source><source src="'+file+'.ogg" type="audio/ogg"></source>');
-            $(audio).attr("src", 'http://api.soundcloud.com/tracks/129642061/stream?client_id=3b7e5fdb483fa306f91111e992c6443f');
-            audio.load();
+            $('audio, video').removeAttr('src').find('source').remove();
+            if( file.indexOf('http')===0 ){
+                $(media).attr("src", file);
+            }else{
+                $(media).append('<source src="'+file+'.mp3" type="video/mpeg"></source><source src="'+file+'.ogg" type="video/ogg"></source>');
+            }
+            try {
+                $(media)[0].load();
+            } catch(e){
+                console.log('NO MEDIA SOURCE! '+e);
+            }
             $('#menu').hide();
             $('#transcript>div').hide().filter('.'+t_id).show();
             $('#track').show();
@@ -130,70 +141,78 @@ jQuery(document).ready(function() {
             );
         $('#ctrls')
             .prepend($('<a class="button" id="list" href="javascript:void(0)">Track list</a>')
-                .on('click', function(){ audio.pause(); $('#track').hide(); $('#menu').show(); })
+                .on('click', function(){ $($('.menu.playing').data('media'))[0].pause(); $('#track').hide(); $('#menu').show(); })
             )
             .append($('<a class="button" id="next" href="javascript:void(0)">Next</a>')
                 .on('click', function(){ 
                     if( $('a.menu.button').last().hasClass('playing') ){
                         $('a.menu.button').first().trigger('click'); 
-                    }else{ $('a.menu.button.playing').parent().next().children('.menu.button').trigger('click'); }
+                    }else{ $('a.menu.button.playing').parent().next().find('.menu.button').trigger('click'); }
                 })
             );
     });
 
     $('#player').each(function(){
-        if( !!document.createElement('audio').canPlayType ){
-            var audio = $('audio')[0],
-                manualSeek = 0,
+        if( !!document.createElement('video').canPlayType ){
+            var manualSeek = 0,
                 vol = 0.7;
             this.reset = function(){
-                audio.pause(); 
-                audio.volume = vol;
+                media = $('audio, video')[0];
+                if( !!media ){
+                    media.pause();
+                    media.volume = vol;
+                }
                 $('#track').hide(); 
                 $('#menu').show();
                 slider.init($('#volknob')[0], { min:0.4, max:1, val:vol, 
-                    slide:function(){ audio.volume = slider.value; }
+                    slide:function(){ $($('.menu.playing').data('media'))[0].volume = slider.value; }
                 });
             };
             this.reset();
-            slider.init($('#progknob')[0], { max:audio.duration, 
-                slide:function(){ manualSeek = 1; },
-                stop:function(){ audio.currentTime = slider.value; manualSeek = 0; }
-            });
-            $(audio)
+            $('audio, video')
                 .on('loadeddata', function(){
-                    var t = parseInt(audio.duration,10),
+                    var t = parseInt(this.duration,10),
                         m = Math.floor(t/60,10),
                         s = t-m*60;
                     $('#time').text((m<10?'0'+m:m)+':'+(s<10?'0'+s:s));
                 })
                 .on('durationchange', function(){
-                    slider.init($('#progknob')[0], { max:audio.duration });
+                    var media = this;
+                    slider.init($('#progknob')[0], { max:media.duration,
+                        slide:function(){ manualSeek = 1; },
+                        stop:function(){ media.currentTime = slider.value; manualSeek = 0; }
+                    });
                 })
                 .on('timeupdate', function(){
-                    var t = audio.currentTime,
-                        t_t = $('div.'+$('a.menu.button.playing').attr('id')).data('times').split(',');
-                    if( !manualSeek ){ $('#progknob').css({left:t*100/audio.duration+'%'}); }
-                    for(var s=0; s<t_t.length; ++s){
-                        if( t >= t_t[s] ){
-                            $('#transcript p').hide();
-                            $('p.'+t_t[s]).show();
+                    var media = this,
+                        t = media.currentTime;
+                    if( !manualSeek ){ $('#progknob').css({left:t*100/media.duration+'%'}); }
+                    var t_t = $('div.'+$('.menu.playing').attr('id')).data('times');
+                    if( !!t_t ){
+                        var t_t = t_t.split(',');
+                        for(var s=0; s<t_t.length; ++s){
+                            if( t >= t_t[s] ){
+                                $('#transcript p').hide();
+                                $('p.'+t_t[s]).show();
+                            }
                         }
                     }
                 })
                 .on('play', function(){ $('#play').addClass('playing'); })
                 .on('pause ended', function(){ $('#play').removeClass('playing'); })
-            ;		
-            $('#play').click(function(){ if( audio.paused ){ audio.play(); }else{ audio.pause(); } });
+            $('#play').on('click', function(){ 
+                var media = $($('.menu.playing').data('media'))[0]; 
+                if( media.paused ){ media.play(); }else{ media.pause(); } 
+            });
         }else{
-            $('audio').attr('controls','controls');
+            $('video').attr('controls','controls');
             $('#dash').hide();
         }
     });
     
     /* timeout screen */
     $.get('./timeout/index.xml', function(xml){
-        var $img = $(xml).children('timeout').children('image');
+        var $img = $(xml).find('timeout').find('image');
         if( $img.length>0 ){
             $img.each(function(){
                 $('#timeout').append($('<img src="./timeout/'+$(this).text()+'" title="Click to begin" alt=""/>').on('error', function(){ $(this).remove(); }));
@@ -202,12 +221,14 @@ jQuery(document).ready(function() {
                 var to = this, t, 
                     e = 4, d = 120; /* e = slide exposure in secs; d = seconds of inactivity before screen times out */
                 this.init = function(){
-                    if( $('audio')[0].paused ){
-                        $(this).show();
-                        $('#player')[0].reset();
-                        if( $(this).children('img').length>1 ){ t = setTimeout(function(){ to.flick(); }, e); }
-                    }else{
-                        this.reset();
+                    if( $('.menu.playing').length ){
+                        if( $($('.menu.playing').data('media'))[0].paused ){
+                            $(this).show();
+                            $('#player')[0].reset();
+                            if( $(this).children('img').length>1 ){ t = setTimeout(function(){ to.flick(); }, e); }
+                        }else{
+                            this.reset();
+                        }
                     }
                 };
                 this.flick = function(){
